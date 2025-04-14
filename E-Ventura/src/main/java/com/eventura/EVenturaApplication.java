4cc395a1949668f4f33e09eb17f238f4d872aa97
thomaplam007 <thongst07vn@gmail.com>
2025-04-14 17:03:24 +0700

    strat project

---
+++ E-Ventura/src/main/java/com/eventura/EVenturaApplication.java
@@ -0,0 +1,13 @@
+package com.eventura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVenturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVenturaApplication.class, args);
+	}
+
+}
