c97eceac3c66403b1ab02bcaff23682dff918fb5
Thong <thongst07vn@gmail.com>
2025-06-10 02:47:44 +0700

    update Admin

--- E-Ventura/src/main/java/com/eventura/controllers/admin/AdminController.java
+++ E-Ventura/src/main/java/com/eventura/controllers/admin/AdminController.java
@@ -1,6 +1,7 @@
 package com.eventura.controllers.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,32 +10,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	
 	@GetMapping({"dashboard"})
-	public String home() {
-		return "admin/dashboard/index";
+	public String home(Model model) {
+        model.addAttribute("currentPage", "dashboard");
+		return "admin/page/dashboard/index";
 	}
 	
+	//======= CATEGORY ========	
 	@GetMapping("category/list")
-	public String categoryList() {
-		return "admin/category/list";
+	public String categoryList(Model model) {
+		model.addAttribute("currentPage", "category");
+		return "admin/page/category/list";
 	}
 	
 	@GetMapping("category/add")
-	public String addCategory() {
-		return "admin/category/add";
+	public String addCategory(Model model) {
+		model.addAttribute("currentPage", "category");
+		return "admin/page/category/add";
+	}
+	@GetMapping("category/edit")
+	public String editCategory(Model model) {
+		model.addAttribute("currentPage", "category");
+		return "admin/page/category/edit";
+	}
+	//======= PRODUCT ========
+	@GetMapping("product/list")
+	public String productList(Model model) {
+		model.addAttribute("currentPage", "product");
+		return "admin/page/product/list";
 	}
 	
-	@GetMapping("order/list")
-	public String orderList() {
-		return "admin/order/list";
+	@GetMapping("product/add")
+	public String productAdd(Model model) {
+		model.addAttribute("currentPage", "product");
+		return "admin/page/product/add";
 	}
 	
-	@GetMapping("order/detail")
-	public String orderDetail() {
-		return "admin/order/detail";
+	@GetMapping("product/edit")
+	public String productEdit(Model model) {
+		model.addAttribute("currentPage", "product");
+		return "admin/page/product/edit";
 	}
 	
-	@GetMapping("order/tracking")
-	public String orderTracking() {
-		return "vendor/order/tracking";
+	//======= USER_CUSTOMER ========
+	@GetMapping("customer/list")
+	public String customerList(Model model) {
+		model.addAttribute("currentPage", "user");
+		return "admin/page/user/customer/list";
+	}
+	//======= USER_VENDOR ========
+	@GetMapping("vendor/list")
+	public String vendorList(Model model) {
+		model.addAttribute("currentPage", "user");
+		return "admin/page/user/vendor/list";
 	}
 }
