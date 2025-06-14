87b7639958a84a81d4aa94a1ae5b0a5149aa42ac
Thong <thongst07vn@gmail.com>
2025-06-14 15:07:04 +0700

    update security

--- E-Ventura/src/main/java/com/eventura/services/AccountServiceImpl.java
+++ E-Ventura/src/main/java/com/eventura/services/AccountServiceImpl.java
@@ -1,5 +1,53 @@
 package com.eventura.services;
 
-public class AccountServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.eventura.entities.Users;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(Users account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Users findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Users findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	
 }
