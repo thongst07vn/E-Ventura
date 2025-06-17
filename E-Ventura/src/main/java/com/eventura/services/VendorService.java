242b589618e5a4b4496bbd05ea0255f1814c48ba
unknown <thongst07vn@gmail.com>
2025-06-18 00:25:24 +0700

    update select product select category findbycate findbyvendor

---
+++ E-Ventura/src/main/java/com/eventura/services/VendorService.java
@@ -0,0 +1,12 @@
+package com.eventura.services;
+
+import java.util.List;
+
+import com.eventura.entities.Vendors;
+
+public interface VendorService {
+	
+	public List<Vendors> findAll();
+
+	
+}
---
+++ E-Ventura/src/main/java/com/eventura/services/VendorServiceImpl.java
@@ -0,0 +1,31 @@
+package com.eventura.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.eventura.entities.Users;
+import com.eventura.entities.Vendors;
+import com.eventura.repositories.VendorRepository;
+
+@Service
+public class VendorServiceImpl implements VendorService {
+
+	@Autowired
+	private VendorRepository vendorRepository;
+	
+	@Override
+	public List<Vendors> findAll() {
+		// TODO Auto-generated method stub
+		return vendorRepository.findAll();
+	}
+
+}
