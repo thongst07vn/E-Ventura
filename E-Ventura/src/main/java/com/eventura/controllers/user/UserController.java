0d538fbd60c85d321156309e5259306679e316e5
trananhkietzxz@gmail.com <trananhkietzxz@gmail.com>
2025-06-19 22:20:41 +0700

    Kiet Tran update product

--- E-Ventura/src/main/java/com/eventura/controllers/user/UserController.java
+++ E-Ventura/src/main/java/com/eventura/controllers/user/UserController.java
@@ -1,12 +1,18 @@
 package com.eventura.controllers.user;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.eventura.entities.ProductCategories;
+import com.eventura.entities.Products;
 import com.eventura.services.CategoryService;
+import com.eventura.services.ProductService;
 
 @Controller
 @RequestMapping({"customer","/"})
@@ -16,9 +22,19 @@ public class UserController {
 	@Autowired
 	private CategoryService categoryService;
 	
+	@Autowired
+	private ProductService productService;
+	
 	@GetMapping({"home","/"})
 	public String home(ModelMap modelMap) {
-		modelMap.put("categories", categoryService.findAll());
+		List<ProductCategories> categories = categoryService.findAll();
+		List<Products> top10products = productService.findTopNewProduct();
+		for(Products pro : top10products) {
+			System.out.println(pro.getName());
+		}
+		modelMap.put("categories", categories);
+		modelMap.put("top10products", top10products);
+		
 		return "customer/pages/home/index";
 	}
 	
